fa0f85ed8fecb917004b75924909d9a8499ed0ed
cggos <cggos@outlook.com>
2022-07-12 16:47:00 +0800

    refactor

--- alg/algorithm.h
+++ alg/algorithm.h
@@ -8,6 +8,12 @@ extern "C" {
 int world_length();
 void test7();
 
+void test_abc();
+void test_bit();
+void test_map();
+void test_lambda_exp();
+void test_precision();
+
 #ifdef __cplusplus
 }
 #endif
