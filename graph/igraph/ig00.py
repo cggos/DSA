380a17ecae35d49d226aeae0086b2b72f23198b6
cggos <cggos@outlook.com>
2023-01-07 08:50:06 +0800

    update graph/igraph/ig00.py

--- graph/igraph/ig00.py
+++ graph/igraph/ig00.py
@@ -2,7 +2,28 @@
 import matplotlib.pyplot as plt
 import igraph as ig
 
+
+# g = ig.Graph.Famous("petersen")
+g = ig.Graph(edges=[[0, 1], [1, 2], [1, 3], [0, 3]])
+vts_label = ['0', '1', '2', '3']
+# g = ig.Graph.Adjacency([[0, 1, 1], [0, 0, 0], [0, 0, 1]])
+
+print(g.summary(verbosity=1))
+
+nv = g.vcount()
+ne = g.ecount()
+
+for v in g.vs:
+    print(v)
+for e in g.es:
+    print(e)
+
+e = g.es[0]
+v1, v2 = e.source, e.target
+
+v = g.vs[0]
+edges = v.incident()
+
 fig, ax = plt.subplots()
-g = ig.Graph.Famous("petersen")
-ig.plot(g, target=ax)
+ig.plot(g, target=ax, vertex_label=vts_label)
 plt.show()
