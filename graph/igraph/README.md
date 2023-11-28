e2bcdab88aa3ade6b3b0e20b0333c120c75f861a
Gavin Gao <cggos@outlook.com>
2023-11-28 22:31:21 +0800

    update igraph files

--- graph/igraph/README.md
+++ graph/igraph/README.md
@@ -1,7 +1,16 @@
 # igraph
 
-* https://igraph.org/
-
 ---
 
+* https://igraph.org/
 
+install
+
+```bash
+pip install python-igraph
+```
+
+for visualization:
+```bash
+pip install cairocffi
+```
--- graph/igraph/ig00.py
+++ graph/igraph/ig00.py
@@ -1,29 +1,32 @@
-
-import matplotlib.pyplot as plt
 import igraph as ig
 
+# import matplotlib.pyplot as plt
 
-# g = ig.Graph.Famous("petersen")
+
 g = ig.Graph(edges=[[0, 1], [1, 2], [1, 3], [0, 3]])
-vts_label = ['0', '1', '2', '3']
+# g = ig.Graph.Famous("petersen")
 # g = ig.Graph.Adjacency([[0, 1, 1], [0, 0, 0], [0, 0, 1]])
+# g = ig.Graph.Tree(127, 2)
 
 print(g.summary(verbosity=1))
 
-nv = g.vcount()
-ne = g.ecount()
+adj_mat = g.get_adjacency()
+print(f"adj mat:\n {adj_mat}")
 
-for v in g.vs:
-    print(v)
-for e in g.es:
-    print(e)
+print(f"nv: {g.vcount()}, ne: {g.ecount()}")
+
+# for v in g.vs:
+#     print(v)
+# for e in g.es:
+#     print(e)
 
 e = g.es[0]
 v1, v2 = e.source, e.target
 
 v = g.vs[0]
-edges = v.incident()
+edges = g.incident(v)
+print(edges)
 
-fig, ax = plt.subplots()
-ig.plot(g, target=ax, vertex_label=vts_label)
-plt.show()
+layout = g.layout(layout='auto')
+vts_label = ['0', '1', '2', '3']
+ig.plot(g, layout="kamada_kawai", vertex_label=vts_label)
